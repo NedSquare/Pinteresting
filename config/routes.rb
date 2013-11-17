4513d1104f40cbbb1c424664db478eedc6cf1f95
email <nednadima@bonaparteimpress.com>
2013-11-17 18:33:47 -0500

    design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
   root 'pages#home'
   get "about" => "pages#about"  #create the about_path
+  get "contact" => "pages#contact" #create the contact_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
