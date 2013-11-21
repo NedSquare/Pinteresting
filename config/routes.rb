9f905a3e78bd7699071e38a46cc927ec3d16bcc8
email <nednadima@bonaparteimpress.com>
2013-11-21 16:36:23 -0500

    added new pages to make it look fancy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Pinteresting::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root 'pages#home'
+  root 'pins#index'
   get "about" => "pages#about"  #create the about_path
   get "contact" => "pages#contact" #create the contact_path
 
